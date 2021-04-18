3e28433c4607688f26d157bd41a4c1bac11d1562
StarWishsama <starwishsama@outlook.com>
2021-04-18 13:22:42 +0800

    功能(JWT): 引入 gf-jwt 的示例 JWT 验证

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/gogf/gf v1.15.6
+	github.com/gogf/gf-jwt v1.1.3 // indirect
 	github.com/gogf/swagger v1.2.0
 )
--- router/router.go
+++ router/router.go
@@ -1,16 +1,24 @@
 package router
 
 import (
+	"github.com/gogf/gf-jwt/example/service"
 	"github.com/yunzhubanban/server/app/api"
 
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
 )
 
+// authHook is the HOOK function implements JWT logistics.
+func middlewareAuth(r *ghttp.Request) {
+	api.Auth.MiddlewareFunc()(r)
+	r.Middleware.Next()
+}
+
 func init() {
 	s := g.Server()
 	s.Group("/apis", func(group *ghttp.RouterGroup) {
 		group.Group("/endpoint/", func(group *ghttp.RouterGroup) {
+			group.Middleware(service.Middleware.CORS, middlewareAuth)
 			group.GET("/service", api.Service)
 		})
 	})
